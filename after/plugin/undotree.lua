74a9fe89daa7fff0fb970cef1e9b6c2be66ee5ad
DanCouper <dan.couper@strivegaming.com>
2025-02-18 23:43:16 +0000

    feat: add snacks, lil bit of fiddling

--- after/plugin/undotree.lua
+++ after/plugin/undotree.lua
@@ -1,1 +1,15 @@
-vim.keymap.set('n', '<leader>u', vim.cmd.UndotreeToggle)
+-- vim:foldmethod=marker
+-- =============================================================================
+-- Undotree settings
+--
+-- For reference, see https://github.com/mbbill/undotree
+-- =============================================================================
+local helpers = require("sutorio.helpers")
+
+helpers.lmap({
+  ["eu"] = {
+    mode = "n",
+    rhs = vim.cmd.UndotreeToggle,
+    desc = "undotree toggle open/close",
+  },
+})
